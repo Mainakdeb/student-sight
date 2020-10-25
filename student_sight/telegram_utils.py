834b1a52be33863a77c81e8a3119a12c2198570b
mayukh <53133634+Mayukhdeb@users.noreply.github.com>
2020-10-25 13:19:16 +0530

    telegram_utils: remove tokens

--- student_sight/telegram_utils.py
+++ student_sight/telegram_utils.py
@@ -3,8 +3,8 @@ import requests
 
 class sight_bot():
     def __init__(self):
-        self.token = '1142444922:AAEk7mqkxbXlVC6TNWSCootQznh_rX_DRWA'  ## bot token 
-        self.chatID = '967526545'
+        self.token = 'your_token_here'  ## bot token 
+        self.chatID = 'your_chatID_here'  ## https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
 
     def send_message(self, message):
         send_text = 'https://api.telegram.org/bot' + self.token + '/sendMessage?chat_id=' + self.chatID + '&parse_mode=Markdown&text=' + message
